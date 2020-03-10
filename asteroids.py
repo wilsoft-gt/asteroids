3ec99e6d05af93c8fae28fcb8e29487473f07161
wilsoft-gt <wilson.romero1992@gmail.com>
2020-03-10 01:05:24 -0600

    everything working

--- asteroids.py
+++ asteroids.py
@@ -9,6 +9,7 @@ from abc import ABC
 from abc import abstractmethod
 import arcade
 import random
+import math
 
 # These are Global constants to use throughout the game
 SCREEN_WIDTH = 800
@@ -35,23 +36,23 @@ SMALL_ROCK_SPIN = 5
 SMALL_ROCK_RADIUS = 2
 
 class Point:
-    def __init__(self, x, y):
-        self.x = x
-        self.y = y
+    def __init__(self):
+        self.x = 0
+        self.y = 0
 
 class Velocity:
-    def __init__(self, dx, dy):
-        self.dx = dx
-        self.dy = dy
+    def __init__(self):
+        self.dx = 0
+        self.dy = 0
 
 class MovingObject(ABC):
-    def __init__(self, centerX, centerY, velocityX, velocityY, lives, angle, radius):
-        self.center = Point(centerY, centerY)
-        self.velocity = Velocity(velocityX, velocityY)
+    def __init__(self):
+        self.center = Point()
+        self.velocity = Velocity()
         self.alive = True
-        self.lives = lives
-        self.radius = radius
-        self.angle = angle
+        self.lives = 0
+        self.radius = 0
+        self.angle = 0
 
     @abstractmethod
     def draw(self):
@@ -83,9 +84,15 @@ class MovingObject(ABC):
 
 
 class LargeAsteroid(MovingObject):
-    def __init__(self, center_x, center_y, velocity_x, velocity_y, lives, angle, radius):
-        super().__init__(center_x, center_y, velocity_x, velocity_y, lives, angle, radius)
-    
+    def __init__(self):
+        super().__init__()
+        self.center.x = random.uniform(0, SCREEN_WIDTH)
+        self.center.y = random.uniform(0, SCREEN_HEIGHT)
+        self.velocity.dx = random.uniform(-BIG_ROCK_SPEED, BIG_ROCK_SPEED)
+        self.velocity.dy = random.uniform(-BIG_ROCK_SPEED, BIG_ROCK_SPEED)
+        self.lives = 3
+        self.radius = BIG_ROCK_RADIUS
+        self.angle = 0
 
     def draw(self):
         large = arcade.load_texture("resources/images/meteorGrey_big1.png")
@@ -97,6 +104,7 @@ class LargeAsteroid(MovingObject):
 
     def rotate(self):
         self.angle += 1
+
     def generate(self):
         pass
 
@@ -109,17 +117,70 @@ class SmallAsteroid(MovingObject):
     pass
 
 class Ship(MovingObject):
-    #TODO implement the rest of the methods
-    def __init__(self, center_x, center_y, velocity_x, velocity_y, lives, angle, radius):
-        super().__init__(center_x, center_y, velocity_x, velocity_y, lives, angle, radius)
+    def __init__(self):
+        super().__init__()
+        self.center.x = SCREEN_WIDTH/2
+        self.center.y = SCREEN_HEIGHT/2
+        self.velocity.dx = 0
+        self.velocity.dy = 0
+        self.lives = 3
+        self.radius = 50
+        self.angle = 0
 
     def draw(self):
-        ship = arcade.load_texture("resources/images/playerSHip1_orange.png")
+        ship = arcade.load_texture("resources/images/playerShip1_orange.png")
         arcade.draw_texture_rectangle(self.center.x, self.center.y, 50, 50, ship, self.angle)
 
+    def advance(self):
+        self.center.x += self.velocity.dx
+        self.center.y += self.velocity.dy
+
+    def rotate_left(self):
+        self.angle += 3 
+
+    def rotate_right(self):
+        self.angle -= 3
+
+    def acelerate(self):
+        self.velocity.dx -= math.sin(math.radians(self.angle)) * SHIP_THRUST_AMOUNT
+        self.velocity.dy += math.cos(math.radians(self.angle)) * SHIP_THRUST_AMOUNT
+
+    def decelerate(self):
+        self.velocity.dx += math.sin(math.radians(self.angle)) * SHIP_THRUST_AMOUNT
+        self.velocity.dy -= math.cos(math.radians(self.angle)) * SHIP_THRUST_AMOUNT
+    
+    def rotate(self):
+        return True
+
 class Bullet(MovingObject):
-    #TODO complete this one
-    pass
+    def __init__(self, ship_angle, ship_x, ship_y):
+        super().__init__()
+        self.center.x = ship_x
+        self.center.y = ship_y
+        self.angle = ship_angle -90
+        self.time = 60
+
+    def draw(self):
+        bullet = arcade.load_texture('resources/images/laserBlue01.png')
+        arcade.draw_texture_rectangle(self.center.x, self.center.y,25,10, bullet, self.angle)
+    
+    def advance(self):
+        self.center.x += self.velocity.dx
+        self.center.y += self.velocity.dy
+
+    def fire(self):
+        self.velocity.dx -= math.sin(math.radians(self.angle+90)) * BULLET_SPEED
+        self.velocity.dy += math.cos(math.radians(self.angle+90)) * BULLET_SPEED
+    
+    def rotate(self):
+        pass
+
+    def times(self):
+        if self.time > 0:
+            self.time -= 1
+        else:
+            self.alive = False
+
 
 
 class Game(arcade.Window):
@@ -143,15 +204,12 @@ class Game(arcade.Window):
 
         self.held_keys = set()
 
-        self.Asteroid1 = LargeAsteroid(SCREEN_WIDTH/2, SCREEN_HEIGHT/2, random.uniform(-BIG_ROCK_SPEED, BIG_ROCK_SPEED), random.uniform(-BIG_ROCK_SPEED, BIG_ROCK_SPEED), 3, 0, BIG_ROCK_RADIUS)
-        self.Asteroid2 = LargeAsteroid(SCREEN_WIDTH/2, SCREEN_HEIGHT/2, random.uniform(-BIG_ROCK_SPEED, BIG_ROCK_SPEED), random.uniform(-BIG_ROCK_SPEED, BIG_ROCK_SPEED), 3, 0, BIG_ROCK_RADIUS)
-        self.Asteroid3 = LargeAsteroid(SCREEN_WIDTH/2, SCREEN_HEIGHT/2, random.uniform(-BIG_ROCK_SPEED, BIG_ROCK_SPEED), random.uniform(-BIG_ROCK_SPEED, BIG_ROCK_SPEED), 3, 0, BIG_ROCK_RADIUS)
-        self.Asteroid4 = LargeAsteroid(SCREEN_WIDTH/2, SCREEN_HEIGHT/2, random.uniform(-BIG_ROCK_SPEED, BIG_ROCK_SPEED), random.uniform(-BIG_ROCK_SPEED, BIG_ROCK_SPEED), 3, 0, BIG_ROCK_RADIUS)
-        self.Asteroid5 = LargeAsteroid(SCREEN_WIDTH/2, SCREEN_HEIGHT/2, random.uniform(-BIG_ROCK_SPEED, BIG_ROCK_SPEED), random.uniform(-BIG_ROCK_SPEED, BIG_ROCK_SPEED), 3, 0, BIG_ROCK_RADIUS)
-
-        self.asteroid_array = [self.Asteroid1, self.Asteroid2, self.Asteroid3, self.Asteroid4, self.Asteroid5]
         
         # TODO: declare anything here you need the game class to track
+        self.ship = Ship()
+        self.asteroid_array = []
+        self.bullets_list = []
+        self.create_asteroids()
 
     def on_draw(self):
         """
@@ -161,9 +219,14 @@ class Game(arcade.Window):
 
         # clear the screen to begin drawing
         arcade.start_render()
+
+        # TODO: draw each object
+        self.ship.draw()
         for asteroid in self.asteroid_array:
             asteroid.draw()
-        # TODO: draw each object
+
+        for bullet in self.bullets_list:
+            bullet.draw()
 
     def update(self, delta_time):
         """
@@ -171,11 +234,21 @@ class Game(arcade.Window):
         :param delta_time: tells us how much time has actually elapsed
         """
         self.check_keys()
+        self.ship.advance()
+        self.ship.touchingEdges()
         for asteroid in self.asteroid_array:
             asteroid.advance()
             asteroid.rotate()
             asteroid.touchingEdges()
 
+        for bullet in self.bullets_list:
+            bullet.advance()
+            bullet.fire()
+            bullet.times()
+            bullet.touchingEdges()
+            if bullet.alive == False:
+                self.bullets_list.remove(bullet)
+        
         # TODO: Tell everything to advance or move forward one step in time
 
         # TODO: Check for collisions
@@ -186,14 +259,18 @@ class Game(arcade.Window):
         You will need to put your own method calls in here.
         """
         if arcade.key.LEFT in self.held_keys:
-            pass
-
+            self.ship.rotate_left()
+            
         if arcade.key.RIGHT in self.held_keys:
-            pass
-
+            self.ship.rotate_right()
+            
         if arcade.key.UP in self.held_keys:
-            pass
-
+            self.ship.acelerate()
+                        
+        if arcade.key.UP not in self.held_keys:
+            self.ship.velocity.dx = 0
+            self.ship.velocity.dy = 0
+                        
         if arcade.key.DOWN in self.held_keys:
             pass
 
@@ -211,8 +288,8 @@ class Game(arcade.Window):
             self.held_keys.add(key)
 
             if key == arcade.key.SPACE:
-                # TODO: Fire the bullet here!
-                pass
+                bullet = Bullet(self.ship.angle, self.ship.center.x, self.ship.center.y)
+                self.bullets_list.append(bullet)
 
     def on_key_release(self, key: int, modifiers: int):
         """
@@ -221,6 +298,17 @@ class Game(arcade.Window):
         if key in self.held_keys:
             self.held_keys.remove(key)
 
+    def create_asteroids(self):
+        Asteroid1 = LargeAsteroid()
+        self.asteroid_array.append(Asteroid1)
+        Asteroid2 = LargeAsteroid()
+        self.asteroid_array.append(Asteroid2)
+        Asteroid3 = LargeAsteroid()
+        self.asteroid_array.append(Asteroid3)
+        Asteroid4 = LargeAsteroid()
+        self.asteroid_array.append(Asteroid4)
+        Asteroid5 = LargeAsteroid()
+        self.asteroid_array.append(Asteroid5)
 
 # Creates the game and starts it going
 window = Game(SCREEN_WIDTH, SCREEN_HEIGHT)
